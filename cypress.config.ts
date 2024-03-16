0dfcf2e23376a94d9715f2dce5eb87d3c055a6af
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-15 17:46:20 -0700

    Integrate TypeScript with Browserify and Cucumber

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "cypress";
+const cucumber = require("cypress-cucumber-preprocessor").default;
+const browserify = require("@cypress/browserify-preprocessor");
+
+export default defineConfig({
+    e2e: {
+        setupNodeEvents(on, config) {
+            const options = {
+                ...browserify.defaultOptions,
+                typescript: require.resolve("typescript"),
+            };
+
+            on("file:preprocessor", cucumber(options));
+        },
+        specPattern: "cypress/e2e/cucumber-tests/*.feature",
+    },
+    video: false,
+    defaultCommandTimeout: 10000,
+    watchForFileChanges: false,
+});
