1bc9f0e59201f330f821f097d83f613d99a67075
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-16 18:49:38 -0700

    Refactor files under user registration step definitions

--- cypress/e2e/cucumber-tests/userRegistration/usedRegisterInput.steps.ts
+++ cypress/e2e/cucumber-tests/userRegistration/usedRegisterInput.steps.ts
@@ -1,10 +1,32 @@
 /// <reference types="cypress" />
 
-import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
 Given("I am on the register page", () => {
     cy.visitRegisterPage();
 });
+
+When(
+    "I enter an email that is already in use while the rest of the fields are filled in appropriately",
+    () => {
+        cy.fixture("registerData").then(
+            ({ nonTakenUsername, takenEmail, password }) => {
+                cy.fillRegisterForm(nonTakenUsername, takenEmail, password);
+            }
+        );
+    }
+);
+
+Then(
+    "I should see an error message indicating the email is already taken",
+    () => {
+        cy.shouldExist(".error-messages");
+        cy.checkTextContent(
+            ".error-messages > li",
+            "email has already been taken"
+        );
+    }
+);
 
 When(
     "I enter a username that is already in use while the rest of the fields are filled in appropriately",
